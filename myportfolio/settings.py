b7c76a240ac678173189e3c99ccf06fd6acf1780
Sagar H N <sagarnagrajgowda0123@gmail.com>
2025-09-19 11:24:18 +0530

    Required changes for versel

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#=jhns+#o327rr$!=xznc6p2*=b85$*%)yc#_h1vn2keq0b1ex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
