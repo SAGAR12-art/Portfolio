4d25b5fbd57db15ff5eeb33eed8941887259bcac
Sagar H N <sagarnagrajgowda0123@gmail.com>
2025-11-04 19:37:40 +0530

    Changes for Ui in media query

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,52 +3,50 @@ import React from "react";
 
 const Education = () => {
   return (
-    <section id="education" className="py-5 bg-light" style={{marginTop:"-200px",marginBottom:"150px"}}>
-      <h2 className="mb-4" data-aos="fade-up" style={{textAlign:"center"}}>
-          Education
-        </h2>
+    <section id="education" className="py-5 bg-light">
+      <h2 className="mb-4" data-aos="fade-up" style={{ textAlign: "center" }}>
+        Education
+      </h2>
+
       <div className="container position-relative">
-        
+        {/* Vertical line (hidden automatically on small screens by CSS) */}
+        <div className="timeline-line" />
 
-        {/* Vertical Line (visible on md and larger screens) */}
-        <div className="timeline-line d-none d-md-block" />
-
-        {/* Item 1 */}
-        <div className="education-item row align-items-center mb-5">
-          <div className="col-md-5 text-end" data-aos="fade-right">
+        {/* Row 1 */}
+        <div className="education-item">
+          <div className="edu-left" data-aos="fade-right">
             <h5><strong>2019</strong></h5>
             <p><strong>Secondary Education</strong></p>
           </div>
 
-          <div className="col-md-2 text-center position-relative">
-            {/* dot is absolutely positioned relative to the .education-item row */}
+          <div className="edu-center">
             <span className="dot" />
           </div>
 
-          <div className="col-md-5 text-start" data-aos="fade-left">
+          <div className="edu-right" data-aos="fade-left">
             <p>MDRS, Baraguru</p>
           </div>
         </div>
 
-        {/* Item 2 */}
-        <div className="education-item row align-items-center mb-5">
-          <div className="col-md-5 text-end" data-aos="fade-right">
+        {/* Row 2 */}
+        <div className="education-item">
+          <div className="edu-left" data-aos="fade-right">
             <h5><strong>2019–2021</strong></h5>
             <p><strong>Pre-University Education</strong></p>
           </div>
 
-          <div className="col-md-2 text-center position-relative">
+          <div className="edu-center">
             <span className="dot" />
           </div>
 
-          <div className="col-md-5 text-start" data-aos="fade-left">
+          <div className="edu-right" data-aos="fade-left">
             <p>BGS PU College, Arkalgud</p>
           </div>
         </div>
 
-        {/* Item 3 */}
-        <div className="education-item row align-items-center mb-5">
-          <div className="col-md-5 text-end" data-aos="fade-right">
+        {/* Row 3 */}
+        <div className="education-item">
+          <div className="edu-left" data-aos="fade-right">
             <h5><strong>2021–2025</strong></h5>
             <p>
               <strong>
@@ -58,11 +56,11 @@ const Education = () => {
             </p>
           </div>
 
-          <div className="col-md-2 text-center position-relative">
+          <div className="edu-center">
             <span className="dot" />
           </div>
 
-          <div className="col-md-5 text-start" data-aos="fade-left">
+          <div className="edu-right" data-aos="fade-left">
             <p>PES College Of Engineering, Mandya</p>
           </div>
         </div>
